b442945b27ce84e6ad8bb2cf52169b1509ab7baf
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-02-19 14:24:20 +0200

    heading update

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { DifferentCards } from '../components/DifferentCards'
+import { styles } from '../styles'
 
 const Content = () => {
   return (
     <div className='py-20 flex flex-col justify-center items-center'>
-       <h1 className="text-xl md:text-3xl lg:text-5xl text-center font-bold py-10">
+          <p className={`${styles.sectionSubText} text-center`}>You may ask...</p>
+          <h1 className="text-xl md:text-3xl lg:text-5xl text-center font-bold py-10">
               What makes us so different?
-        </h1>
-       <DifferentCards />
+          </h1><DifferentCards />
     </div>
   )
 }
