c8e777c4b9b72b78c620557e1c748b9f7736817a
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-02-23 15:37:43 +0200

    theme change

--- src/styles.js
+++ src/styles.js
@@ -13,7 +13,7 @@ const styles = {
       "text-secondary font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
   
     sectionHeadText:
-      "text-tertiary font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+      "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
       sectionHeadTextContact:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionHeadTextContact:
