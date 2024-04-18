0931c4e80a137cd314b7b53a3f71b0d91ac89f73
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-04-18 14:56:50 +0200

    updated project structure to include google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { useEffect, useLayoutEffect } from "react";
 import MainLayout from "./layout/MainLayout";
 import Error from "./layout/Error";
@@ -12,25 +12,31 @@ const TRACKING_ID = "G-51CKGMHRR9";
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
-
-useEffect(() => {
-  const trackPageView = (location) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  };
-})
   return (
     <MantineProvider>
       <BrowserRouter>
-          <Routes>
-            <Route render={({ location }) => trackPageView(location)} />
-            <Route path="/" element={<MainLayout />} />
-            <Route path="/blog" element={<BlogLayout />} /> 
-            <Route path="/blog/:slug" element={<FullBlogPost />} /> 
-            <Route path="*" element={<Error />} />
-          </Routes> 
+          <TrackingRoutes />
       </BrowserRouter>
     </MantineProvider>
+  );
+}
+
+function TrackingRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Track page view whenever location changes
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />} />
+      <Route path="/blog" element={<BlogLayout />} />
+      <Route path="/blog/:slug" element={<FullBlogPost />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
