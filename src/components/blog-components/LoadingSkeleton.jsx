aeb05c41906bff9e212c5931ad32f2490e811fe2
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-05-11 13:42:31 +0200

    updated loader

--- src/components/blog-components/LoadingSkeleton.jsx
+++ src/components/blog-components/LoadingSkeleton.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 const LoadingSkeleton = () => {
   return (
     <div className='min-h-[40vh] flex flex-col mx-auto justify-center'>
-        <div className='flex justify-around text-center items-center'>
-            <h1 className='hidden md:block text-3xl font-bold'>Loading...</h1>
+        <div className='flex flex-col gap-6 justify-start text-center items-center'>
             <Loader size={50}  />
+            <h1 className='hidden md:block text-3xl font-bold'>Loading...</h1>
         </div>
     </div>
   )
