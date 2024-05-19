5e7ca69d4c378203f04fe7b518890feaee712da8
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-05-19 20:59:16 +0200

    why is this so fucken hard

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  }
 })
