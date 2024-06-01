b8fee2b11fcf044259979d558fa919a477362405
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-06-01 14:48:18 +0200

    updating background to not be so resource intensive

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Hero from '../pages/Hero'
 import Main from '../pages/Main'
 import Content from '../pages/Content'
 import Footer from '../pages/Footer'
 import { Contact } from '../pages/Contact'
+import { Background } from '../pages/Background'
 
 const MainLayout = () => {
   return (
     <div className="z-0 bg-[#152238] text-white">
-            <Hero />
+            <Background />
             <Main />
             <Content />
             <Contact />
---
+++ src/pages/Background.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { HeroHeading } from "../components/HeroHeading";
+ 
+export function Background() {
+  return (
+    <div className="z-0 min-h-screen w-full bg-slate-900 bg-dot-white/[0.4] relative flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-slate-900  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <Navbar />
+      <HeroHeading />
+    </div>
+  );
+}
