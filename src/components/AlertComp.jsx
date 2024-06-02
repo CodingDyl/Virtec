c905bdca790da04b2b0dbb56501fbda9fdab719e
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-06-02 20:49:39 +0200

    updated the alert to be filled color

--- src/components/AlertComp.jsx
+++ src/components/AlertComp.jsx
@@ -6,7 +6,7 @@ const AlertComp = ({msg, col, title}) => {
     const icon = <IconInfoCircle />
   return (
     <div className="fixed top-2 w-full z-50">
-    <Alert variant="light" color={col} title={title} icon={icon} className='mx-auto w-[70%]'>
+    <Alert variant="filled" color={col} title={title} icon={icon} className='mx-auto w-[70%]'>
       <p className='text-white'>{msg}</p>
     </Alert>
     </div>
